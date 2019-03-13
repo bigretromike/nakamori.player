e7fa594de4674d1f99ba40b3a771f73ac797bece
da3dsoul <da3dsoul@gmail.com>
2019-03-13 19:48:50 -0400

    More refactoring. Add Voting. Filter URLs from descriptions. More work on the error handler.

--- lib/nakamori_player.py
+++ lib/nakamori_player.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
-import json
-
+import nakamori_utils.shoko_utils
 import xbmcgui
-from nakamori_utils import nakamoritools as nt, model_utils, infolabel_utils, kodi_utils
+from nakamori_utils import nakamoritools as nt, infolabel_utils
 from nakamori_utils.globalvars import *
 from threading import Thread
 
 from proxy.kodi_version_proxy import kodi_proxy
 from proxy.python_version_proxy import python_proxy as pyproxy
-from error_handler import log
+import error_handler as eh
+from error_handler import log, ErrorPriority
 
 busy = xbmcgui.DialogProgress()
 
@@ -42,7 +42,7 @@ def finished_episode(ep_id, current_time, total_time):
     if _finished:
         if int(ep_id) != 0 and plugin_addon.getSetting('vote_always') == 'true':
             # convert in case shoko give float
-            nt.vote_episode(ep_id)
+            nakamori_utils.shoko_utils.vote_episode(ep_id)
         from shoko_models.v2 import Episode
         ep = Episode(ep_id, build_full_object=False)
         ep.set_watched_status(True)
@@ -56,6 +56,7 @@ def play_video(file_id, ep_id=0, mark_as_watched=True, resume=False):
     Plays a file
     :param file_id: file ID. It is needed to look up the file
     :param ep_id: episode ID, not needed, but it fills in a lot of info
+    :param mark_as_watched: showld we mark it after playback
     :param resume: should we auto-resume
     :return: True if successfully playing
     """
@@ -139,8 +140,7 @@ def process_transcoder(file_id, file_url, file_obj):
     try:
         eigakan_data = pyproxy.get_json(eigakan_host + '/api/version')
         if 'eigakan' not in eigakan_data:
-            nt.error('Eigakan server is unavailable')
-            return is_transcoded, m3u8_url
+            raise RuntimeError('Invalid response from Eigakan')
 
         audio_stream_id = find_language_index(file_obj.audio_streams, plugin_addon.getSetting('audiolangEigakan'))
         sub_stream_id = find_language_index(file_obj.sub_streams, plugin_addon.getSetting('subEigakan'))
@@ -190,8 +190,8 @@ def process_transcoder(file_id, file_url, file_obj):
         if pyproxy.head(url_in=ts_url):
             is_transcoded = True
 
-    except Exception as exc:
-        nt.error('eigakan.post_json nt.error', str(exc))
+    except:
+        eh.exception(ErrorPriority.BLOCKING)
         busy.close()
 
     return is_transcoded, m3u8_url
@@ -215,139 +215,6 @@ def find_language_index(streams, setting):
         if stream_id != -1:
             break
     return stream_id
-
-
-def play_video_old(ep_id, raw_id, movie):
-    """
-    Plays a file or episode
-    Args:
-        ep_id: episode id, if applicable for watched status and stream details
-        raw_id: file id, that is only used when ep_id = 0
-        movie: determinate if played object is movie or episode (ex.Trakt)
-    Returns:
-
-    """
-    details = {
-        'plot':          xbmc.getInfoLabel('ListItem.Plot'),
-        'title':         xbmc.getInfoLabel('ListItem.Title'),
-        'sorttitle':     xbmc.getInfoLabel('ListItem.Title'),
-        'rating':        xbmc.getInfoLabel('ListItem.Rating'),
-        'duration':      xbmc.getInfoLabel('ListItem.Duration'),
-        'mpaa':          xbmc.getInfoLabel('ListItem.Mpaa'),
-        'year':          xbmc.getInfoLabel('ListItem.Year'),
-        'tagline':       xbmc.getInfoLabel('ListItem.Tagline'),
-        'episode':       xbmc.getInfoLabel('ListItem.Episode'),
-        'aired':         xbmc.getInfoLabel('ListItem.Premiered'),
-        'tvshowtitle':   xbmc.getInfoLabel('ListItem.TVShowTitle'),
-        'votes':         xbmc.getInfoLabel('ListItem.Votes'),
-        'originaltitle': xbmc.getInfoLabel('ListItem.OriginalTitle'),
-        'size':          xbmc.getInfoLabel('ListItem.Size'),
-        'season':        xbmc.getInfoLabel('ListItem.Season'),
-        'epid':          ep_id,  # player
-        'movie':         movie,  # player
-        'fileid':        0,      # player, coded below
-        'rawid':         raw_id  # player
-    }
-
-    file_id = ''
-    file_url = ''
-    file_body = None
-    item = ''
-
-    resume = plugin_addon.getSetting('resume') == '1'
-    plugin_addon.setSetting('resume', '0')
-
-    try:
-        if ep_id != '0':
-            episode_url = server + '/api/ep?id=' + str(ep_id)
-            episode_url = pyproxy.set_parameter(episode_url, 'level', '1')
-            html = pyproxy.get_json(pyproxy.encode(episode_url))
-            if plugin_addon.getSetting('spamLog') == 'true':
-                xbmc.log(html, xbmc.LOGWARNING)
-            episode_body = json.loads(html)
-            if plugin_addon.getSetting('pick_file') == 'true':
-                file_id = kodi_utils.file_list_gui(episode_body)
-            else:
-                file_id = episode_body['files'][0]['id']
-        else:
-            file_id = raw_id
-
-        if file_id is not None and file_id != 0:
-            details['fileid'] = file_id
-            file_url = server + '/api/file?id=' + str(file_id)
-            file_body = json.loads(pyproxy.get_json(file_url))
-
-            file_url = file_body['url']
-            server_path = file_body.get('server_path', '')
-            if server_path is not None and server_path != '':
-                try:
-                    if os.path.isfile(server_path):
-                        if server_path.startswith(u'\\\\'):
-                            server_path = u'smb:'+server_path
-                        file_url = server_path
-                except:
-                    pass
-
-            # Information about streams inside video file
-            # Video
-            codecs = dict()
-            model_utils.video_file_information(file_body['media'], codecs)
-            details['path'] = file_url
-            details['duration'] = file_body.get('duration', 0)
-            details['size'] = file_body['size']
-
-            item = xbmcgui.ListItem(details.get('title', 'Unknown'),
-                                    thumbnailImage=xbmc.getInfoLabel('ListItem.Thumb'),
-                                    path=file_url)
-            item.setInfo(type='Video', infoLabels=details)
-
-            if 'offset' in file_body:
-                offset = file_body.get('offset', 0)
-                if offset != 0:
-                    offset = int(offset) / 1000
-                    if plugin_addon.getSetting('file_resume') == 'true' and resume:
-                        item.setProperty('ResumeTime', str(offset))
-                        item.setProperty('StartOffset', str(offset))
-
-            for stream_index in codecs['VideoStreams']:
-                if not isinstance(codecs['VideoStreams'][stream_index], dict):
-                    continue
-                item.addStreamInfo('video', codecs['VideoStreams'][stream_index])
-            for stream_index in codecs['AudioStreams']:
-                if not isinstance(codecs['AudioStreams'][stream_index], dict):
-                    continue
-                item.addStreamInfo('audio', codecs['AudioStreams'][stream_index])
-            for stream_index in codecs['SubStreams']:
-                if not isinstance(codecs['SubStreams'][stream_index], dict):
-                    continue
-                item.addStreamInfo('subtitle', codecs['SubStreams'][stream_index])
-        else:
-            if plugin_addon.getSetting('pick_file') == 'false':
-                nt.error('file_id not retrieved')
-            return 0
-    except Exception as exc:
-        nt.error('util.nt.error getting episode info', str(exc))
-
-    is_transcoded, m3u8_url = process_transcoder(file_id, file_url)
-
-    player = Player()
-    player.feed(file_id, ep_id, details.get('duration', 0), m3u8_url if is_transcoded else file_url, True)
-
-    try:
-        if is_transcoded:
-            details['path'] = m3u8_url
-            player.feed(file_id, ep_id, details.get('duration', 0), m3u8_url if is_transcoded else file_url, True)
-            player.play(item=m3u8_url)
-        else:
-            player.play(item=file_url, listitem=item)
-
-    except Exception as player_ex:
-        xbmc.log('---> player_ex: ' + str(player_ex), xbmc.LOGWARNING)
-
-    # leave player alive so we can handle onPlayBackStopped/onPlayBackEnded
-    xbmc.sleep(int(plugin_addon.getSetting('player_sleep')))
-
-    return player_loop(player)
 
 
 # noinspection PyUnusedFunction
